790844d1650a9c5e044c1ecc01638f9a386da73a
Ujjwal Gulecha <ujjwal.gulecha@gmail.com>
2017-03-21 19:01:48 -0700

    Day 15

---
+++ Day_15/Part_1.py
@@ -0,0 +1,33 @@
+ing = []
+
+with open("Day_15.input") as f:
+	for line in f:
+		data = line.split()
+		dat = [int(data[2][:-1]), int(data[4][:-1]), int(data[6][:-1]), int(data[8][:-1]), int(data[10])]
+		ing.append(dat)
+
+max_score = float("-inf")
+total_weight = 100
+
+def get_score(i, j, k):
+	rem = 100 - (i + j + k)
+	mult = [i, j, k, rem]
+	score = 1
+	for x in range(len(ing[0])):
+		#not counting calories
+		if x == 4:
+			continue
+		sc = 0
+		for y in range(len(ing)):
+			sc+= (ing[y][x]* mult[y])
+		if sc < 0:
+			score = 0
+		score = score * sc
+	return score
+
+for i in range(0, total_weight + 1):
+	for j in range(0, total_weight - i +1):
+		for k in range(0, total_weight - i - j + 1):
+			max_score = max(max_score, get_score(i, j, k))
+		
+print max_score
